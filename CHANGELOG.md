f592a8ce14e920933f148f9783bb31ad16f1b20b
Vasily Mikhaylichenko <vasily.mikhaylichenko@gmail.com>
2013-06-17 10:01:55 +1000

    Version bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
-## 0.1 / 2013-06-12
+## 0.1.1 / 2013-06-17
+Depend on yum cookbook.
 
+## 0.1 / 2013-06-12
 Initial release.
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'vasily@locomote.com'
 license          'BSD'
 description      'Installs Siege'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1'
+version          '0.1.1'
 
 %w{centos redhat ubuntu gentoo}.each do |os|
   supports os
